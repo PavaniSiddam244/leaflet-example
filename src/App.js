436303576568e4de5ce0bc6dce85c9fdb41308d7
gdornala <gdornala@apple.com>
2019-02-26 15:55:46 +0530

    basic app is created

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'leaflet/dist/leaflet.css';
+
+import LeafletMap from './components/map';
+import Header from './components/header';
+
+class App extends PureComponent {
+  state = {
+    markers: [],
+  };
+
+  setMarker = ({latitude, longitude}) => {
+    this.setState({
+      markers: [...this.state.markers, {
+        latitude,
+        longitude 
+      }]
+    })
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App"style={{textAlign: "left"}}>
+        <Header setMarker={this.setMarker} />
+        <LeafletMap markers={this.state.markers} />
+      </div>
+    );
+  }
+}
+
+export default App;
